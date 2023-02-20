42cd9e515f967241993aec395c978982057d1375
bujosa <davidbujosa@gmail.com>
2023-02-19 21:45:18 -0400

    feat: create simple mock login

--- lib/presentations/pages/login.dart
+++ lib/presentations/pages/login.dart
@@ -1,16 +1,95 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Login Page'),
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(top: 200, bottom: 150),
+              child: Text('LOG IN',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              padding: const EdgeInsets.only(left: 10, top: 5),
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+              ),
+              child: const TextField(
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Username',
+                    hintStyle: TextStyle(color: Colors.black)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                padding: const EdgeInsets.only(left: 10, top: 5),
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                ),
+                child: TextField(
+                  obscureText: !_passwordVisible,
+                  style: const TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: InputBorder.none,
+                    hintStyle: const TextStyle(color: Colors.black),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              padding: const EdgeInsets.only(left: 10),
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+              ),
+              child: const Center(
+                  child: Text('Log In',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black))),
+            ),
           ],
         ),
       ),
