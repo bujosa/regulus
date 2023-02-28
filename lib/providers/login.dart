c9c38a4a27da0496a47cfded8150251b844eee1b
bujosa <davidbujosa@gmail.com>
2023-02-28 01:49:10 -0400

    feat: add login provider

---
+++ lib/providers/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginProvider extends ChangeNotifier {
+  bool passwordVisible = false;
+
+  void changePasswordProvider() {
+    passwordVisible != passwordVisible;
+
+    notifyListeners();
+  }
+}
